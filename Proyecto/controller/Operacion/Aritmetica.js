ae4f7da4d2e70718ed4e50ea617e9f730268f412
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2021-10-10 00:30:19 -0600

    Resta

--- Proyecto/controller/Operacion/Aritmetica.js
+++ Proyecto/controller/Operacion/Aritmetica.js
@@ -105,7 +105,51 @@ function resta(_opIzq, _opDer, _ambito){
     const tipoRes = TipoResultado(opIzq.tipo, opDer.tipo)
     if(tipoRes!=null){
         if(tipoRes === TIPO_DATO.DECIMAL){
-            const resultado = Number(opIzq.valor) - Number(opDer.valor);
+
+            num1 = opIzq.valor
+            num2 = opDer.valor
+
+            split1 = String(_opIzq.valor).split("\'")
+
+            if(split1.length > 1){
+                num1 = split1[1].charCodeAt(0)
+            }
+
+            split1 = String(_opDer.valor).split("\'")
+
+            if(split1.length > 1){
+                num2 = split1[1].charCodeAt(0)
+            }
+
+            const resultado = Number(num1) - Number(num2);
+
+            return{
+                valor: resultado,
+                tipo: tipoRes,
+                linea: _opIzq.linea,
+                columna: _opIzq.columna
+            }
+        }
+
+        else if(tipoRes === TIPO_DATO.ENTERO){
+
+            num1 = opIzq.valor
+            num2 = opDer.valor
+
+            split1 = String(_opIzq.valor).split("\'")
+
+            if(split1.length > 1){
+                num1 = split1[1].charCodeAt(0)
+            }
+
+            split1 = String(_opDer.valor).split("\'")
+
+            if(split1.length > 1){
+                num2 = split1[1].charCodeAt(0)
+            }
+
+            const resultado = Number(num1) - Number(num2);
+
             return{
                 valor: resultado,
                 tipo: tipoRes,
@@ -116,7 +160,7 @@ function resta(_opIzq, _opDer, _ambito){
     }
     var respuesta = (opIzq.tipo===null ? opIzq.valor: "")+(opDer.tipo===null ? opDer.valor: "") //true+5+10+5
     return{
-        valor: respuesta+'\nError semántico: no se puede realizar la operacion suma... Linea: '+_opIzq.linea+" Columna: "+_opIzq.columna,
+        valor: respuesta+'\nError semántico: no se puede realizar la operacion resta... Linea: '+_opIzq.linea+" Columna: "+_opIzq.columna,
         tipo: null,
         linea: _opIzq.linea,
         columna: _opIzq.columna
